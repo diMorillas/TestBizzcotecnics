548fbfa5109ee5345552fb49dafb3d81f7504df3
diMorillas <didacmorillas19@gmail.com>
2025-03-14 18:57:20 +0100

    Added first display:none

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    //DOM básico
+    let pageTest = document.getElementById('pageTest');
+
+
     // Testing conexión con archivo JavaScript
     console.log('Successfully Connected to JavaScript File');
 
-    //Gestión de eventos en el body
-    document.body.addEventListener('click', ()=>{
+    // Gestión de eventos en el body
+    document.body.addEventListener('click', () => {
         playMusic();
     });
 
@@ -12,34 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let music = document.getElementById('bg-music');
     const playMusic = () => {
         music.play()
-            .then(() => {
-                console.log('Música inicializada correctamente');
-            })
-            .catch((error) => {
-                console.error('Error al inicializar música:', error);
-            });
     };
 
     // Cuenta regresiva
-    const timeRemainingTest =()=>{
+    const timeRemainingTest = () => {
         let timer = document.getElementById('timer');
-        let timeRemaining = 120;  
-    
+        let timeRemaining = 30;
+
         const countdown = setInterval(() => {
-            timeRemaining--; 
-    
+            timeRemaining--;
+            console.log(timeRemaining);
+
             timer.textContent = timeRemaining;
 
-        if (timeRemaining <= 15) {
-            timer.style.color = 'red'; 
-            document.body.classList.add('timeout'); 
-        } else {
-            timer.style.color = 'white'; 
-            document.body.classList.remove('timeout'); 
-        }    
+            if (timeRemaining <= 15) {
+                timer.style.color = 'red';
+                document.body.classList.add('timeout');
+            } else {
+                timer.style.color = 'white';
+                document.body.classList.remove('timeout');
+            }
+
             if (timeRemaining <= 0) {
                 clearInterval(countdown);
-                console.log('Cuenta regresiva terminada de manera satisfactoria');
+                console.log('Cuenta regresiva terminada for limite de tiempo');
             }
         }, 1000);
     }
@@ -54,12 +54,20 @@ document.addEventListener('DOMContentLoaded', () => {
     // Gestión Test Dinámico Botón
     let nextTest = document.getElementById('btnNext');
 
-    nextTest.addEventListener('click',()=>{
+    nextTest.addEventListener('click', () => {
         testCounter < 3 ? testCounter++ : console.error('Máximos test por jugador alcanzado');
         numberTest.innerHTML = `${testCounter}/3`;
+        console.log(testCounter);
+
+        // Gestión de SPA (se evalúa cada vez que cambia el contador)
+        if (testCounter == 3) {
+            alert('Has completado el test.');
+            pageTest.style.display = 'none';
+            clearInterval(countdown);
+            console.log('Cuenta regresiva terminada de manera satisfactoria');
+        } else {
+            pageTest.style.display = 'block';
+        }
     });
-
-
-
 
 });
