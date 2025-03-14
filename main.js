3e414687b5d7ba5e1d50e3f654f62c732a0cb3b2
diMorillas <didacmorillas19@gmail.com>
2025-03-14 17:24:49 +0100

    Added music

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     // Testing conexión con archivo JavaScript
-    console.log('Succesfully Connected');
+    console.log('Successfully Connected');
+
+    // Gestión de música
+    let music = document.getElementById('bg-music');
+    const playMusic = () => {
+        music.play()
+            .then(() => {
+                console.log('Música inicializada correctamente');
+            })
+            .catch((error) => {
+                console.error('Error al inicializar música:', error);
+            });
+    };
+    document.body.addEventListener('click', playMusic);
 
 
 
