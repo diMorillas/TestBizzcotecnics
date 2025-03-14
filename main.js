5735a41fa3844c38b396a58d0681ebe8519743ce
diMorillas <didacmorillas19@gmail.com>
2025-03-14 17:44:16 +0100

    Added some styles

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Testing conexión con archivo JavaScript
-    console.log('Successfully Connected');
+    console.log('Successfully Connected to JavaScript File');
+
+    //Gestión de eventos en el body
+    document.body.addEventListener('click', ()=>{
+        playMusic();
+    });
 
     // Gestión de música
     let music = document.getElementById('bg-music');
@@ -14,7 +19,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 console.error('Error al inicializar música:', error);
             });
     };
-    document.body.addEventListener('click', playMusic);
+
+    // Cuenta regresiva
+    const timeRemainingTest =()=>{
+        let timer = document.getElementById('timer');
+        let timeRemaining = 120;  
+    
+        const countdown = setInterval(() => {
+            timeRemaining--; 
+    
+            timer.textContent = timeRemaining;
+
+            timeRemaining <= 15 ? timer.style.color = 'red' : timer.style.color = ' white';
+    
+            if (timeRemaining <= 0) {
+                clearInterval(countdown);
+                console.log('Cuenta regresiva terminada de manera satisfactoria');
+            }
+        }, 1000);
+    }
+    timeRemainingTest();
+
 
 
 
