0f792bfea5184bc0389e21354d481035259faa55
dimori2425 <15587067.clot@fje.edu>
2025-03-16 00:29:50 +0100

    Now it doesnt dissapear the figure when you swap to other option

--- drag.js
+++ drag.js
@@ -1,26 +1,34 @@
+// drag.js
 export function initDragAndDrop() {
     const options = document.querySelectorAll('.optionSelected');
     const figureFour = document.querySelector('.figureFour');
   
     figureFour.addEventListener('dragover', (e) => {
-      e.preventDefault(); 
+      e.preventDefault();
     });
   
     figureFour.addEventListener('drop', (e) => {
       e.preventDefault();
-      const draggedElement = document.querySelector('.dragging'); 
+      const draggedElement = document.querySelector('.dragging');
   
       if (draggedElement) {
-        figureFour.appendChild(draggedElement);
-        draggedElement.classList.remove('dragging'); 
+        const existingImage = figureFour.querySelector('.optionSelected');
   
-        draggedElement.style.width = '160px'; 
+        if (existingImage) {
+          figureFour.removeChild(existingImage);
+        }
+  
+        // Clonamos el elemento para no moverlo
+        const clonedElement = draggedElement.cloneNode(true);
+        figureFour.appendChild(clonedElement);
+        clonedElement.classList.remove('dragging');
+        clonedElement.style.width = '160px'; 
       }
     });
   
     options.forEach(option => {
       option.addEventListener('dragstart', (e) => {
-        e.target.classList.add('dragging'); 
+        e.target.classList.add('dragging');
       });
   
       option.addEventListener('dragend', (e) => {
