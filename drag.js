1d045cb7521a6286f3ef2cf35258c119d47658ec
dimori2425 <15587067.clot@fje.edu>
2025-03-16 00:34:23 +0100

    Now they have value

--- drag.js
+++ drag.js
@@ -23,10 +23,28 @@ export function initDragAndDrop() {
         figureFour.appendChild(clonedElement);
         clonedElement.classList.remove('dragging');
         clonedElement.style.width = '160px'; 
+  
+        const optionValue = draggedElement.dataset.optionValue; 
+        clonedElement.setAttribute('data-option', optionValue); 
       }
     });
   
     options.forEach(option => {
+      switch (option.closest('div').classList[0]) {
+        case 'optionOne':
+          option.dataset.optionValue = 'a';
+          break;
+        case 'optionTwo':
+          option.dataset.optionValue = 'b';
+          break;
+        case 'optionThree':
+          option.dataset.optionValue = 'c';
+          break;
+        case 'optionFour':
+          option.dataset.optionValue = 'd';
+          break;
+      }
+  
       option.addEventListener('dragstart', (e) => {
         e.target.classList.add('dragging');
       });
