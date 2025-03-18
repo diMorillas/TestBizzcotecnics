da86c16a0a04272b1cd521e6a1c0f33e4c903733
diMorillas <didacmorillas19@gmail.com>
2025-03-18 12:57:30 +0100

    Added some changes, RegEx now is needed

--- functions.js
+++ functions.js
@@ -1,13 +1,28 @@
 import { Jugador } from "./Jugador.js";
 
-
 export function addPlayer(array) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/;
+    
     let nombre = prompt('Nombre del jugador');
-    let email = prompt('Email del jugador');
-    let tiempo = prompt('Tiempo en hacer el test');
-    let puntuacio = prompt('Puntuacion sobre 10');
+    if (!nameRegex.test(nombre)) {
+        alert("El nombre solo puede contener letras y no puede estar vacío, saliendo del sistema.");
+        return;
+    }
 
-    let newPlayer = new Jugador(nombre, email, parseInt(tiempo), parseInt(puntuacio));
+    let email = prompt('Email del jugador');
+    
+    if (!emailRegex.test(email)) {  // Corregido: Usar 'email' en la validación
+        alert("Introduce un email válido con '@', saliendo del sistema.");
+        return array;  // Devolver el array sin cambios
+    }
+
+
+
+    let tiempo = Number(prompt('Tiempo en hacer el test'));
+    let puntuacio = Number(prompt('Puntuacion sobre 10'));
+
+    let newPlayer = new Jugador(nombre, email, tiempo, puntuacio);
     array.push(newPlayer);
 
     return array;
