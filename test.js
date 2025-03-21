d62fc69c2951195f6bba3333bf644c11133fb813
rekloyd <paumorillasrivera129@gmail.com>
2025-03-21 17:42:07 +0100

    added new target to manage error

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 export class Test {
-    constructor(figuras,idTest,puntuacion){
+    constructor(figuras, idTest, puntuacion) {
+        if (new.target === Test) {
+            throw new TypeError("No se puede instanciar la clase abstracta Test directamente");
+        }
         this.figuras = figuras;
         this.idTest = idTest;
         this.puntuacion = puntuacion;
-
     }
 }
+
